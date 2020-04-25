c54e9a3dc61453ee58dccd42dc426af06cae6ec2
Mark Hargreaves <clashclanacc2602@gmail.com>
2020-04-25 10:31:48 -0400

    add things, starting the recompiler over

--- cheep.go
+++ cheep.go
@@ -4,6 +4,8 @@ package main
 import(
 	"os"
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 var nnn uint16
@@ -24,6 +26,29 @@ type Cheep8 struct {
 	dT byte
 	sT byte
 }
+func ldfont(chp *Cheep8) error{
+	font := []byte{0xF0, 0x90, 0x90, 0x90, 0xF0,
+		0x20, 0x60, 0x20, 0x20, 0x70,
+		0xF0, 0x10, 0xF0, 0x80, 0xF0,
+		0xF0, 0x10, 0xF0, 0x10, 0xF0,
+		0x90, 0x90, 0xF0, 0x10, 0x10,
+		0xF0, 0x80, 0xF0, 0x10, 0xF0,
+		0xF0, 0x80, 0xF0, 0x90, 0xF0,
+		0xF0, 0x10, 0x20, 0x40, 0x40,
+		0xF0, 0x90, 0xF0, 0x90, 0xF0,
+		0xF0, 0x90, 0xF0, 0x10, 0xF0,
+		0xF0, 0x90, 0xF0, 0x90, 0x90,
+		0xE0, 0x90, 0xE0, 0x90, 0xE0,
+		0xF0, 0x80, 0x80, 0x80, 0xF0,
+		0xE0, 0x90, 0x90, 0x90, 0xE0,
+		0xF0, 0x80, 0xF0, 0x80, 0xF0,
+		0xF0, 0x80, 0xF0, 0x80, 0x80}
+	for count, bt := range font {
+		chp.mem[count] = uint8(bt)
+	}
+	fmt.Println(chp.mem)
+	return nil
+}
 func checkForErr(err error){
 	if err != nil{
 		panic("cheepcheep: error")
@@ -36,9 +61,19 @@ func loadROM(rom string, chp *Cheep8){
 	temp := make([]byte, 4096)
 	n, err := dat.Read(temp)
 	checkForErr(err)
+	ldfont(chp)
 	fmt.Println("debug: got", n, "bytes of data")
 	for i := 0; i < 4096-512; i++ { // some space for a compiler
 		chp.mem[i+int(chp.pc)] = temp[i]	
+	}
+}
+
+func updateTimers(chp *Cheep8){
+	if chp.dT > 0{
+		chp.dT--
+	}
+	if chp.sT > 0{
+		chp.sT--
 	}
 }
 
@@ -48,6 +83,7 @@ func Process(chp *Cheep8){
 	chp.opc |= uint16(chp.mem[chp.pc+1])
 	Interpret(chp)
 	chp.pc +=  2
+	updateTimers(chp)
 	
 }
 
@@ -59,6 +95,11 @@ func Interpret(chp *Cheep8){
 	y = (opcode & 0x00F0) >> 4
 	kk = opcode & 0x00FF
 	fmt.Printf("opcode:0x%X\n", opcode)
+	if opcode == 0x00EE{
+		chp.pc = chp.stack[chp.sp]
+		chp.sp -= 1
+		return
+	}
 	switch opcode & 0xF000{ // get only first one
 	case 0x6000:
 		chp.registers[x] = uint8(kk)
@@ -70,9 +111,9 @@ func Interpret(chp *Cheep8){
 		// skip drawing
 		break
 	case 0x2000: // call subroutine at nnn
+		chp.sp++
 		chp.stack[chp.sp] = chp.pc
 		chp.pc = nnn - 2
-		chp.sp++
 	case 0xF000:
 		switch opcode & 0x00FF {
 		case 0x0033: // store BCD representation of Vx at [i], [i+1], [i+2]
@@ -80,11 +121,36 @@ func Interpret(chp *Cheep8){
 			chp.mem[chp.i] = opreg / 100
 			chp.mem[chp.i+1] = (opreg / 10) % 10
 			chp.mem[chp.i+2] = (opreg % 100) / 10
+		case 0x0007:
+			chp.registers[x] = chp.dT
+		case 0x0015:
+			chp.dT = chp.registers[x]
+		case 0x0065:
+			for V := 0; V < 16; V++ {
+				chp.registers[V] = chp.mem[uint16(V)+chp.i]
+			}
+		case 0x0029:
+			chp.i = uint16(chp.registers[x]) * 5  // we have sprites starting at 0x0, 5B each
+		default:
+			hexopc := fmt.Sprintf("%x", opcode)
+			fmt.Println("cheepcheep: bad opcode", hexopc)
+			panic("cheepcheep: bad opcode")
 		}
 	case 0x7000:
 		if (jit){
 			AssembleAddition(x, kk, chp)
+		}else{
+			chp.registers[x] += uint8(kk)
 		}
+	case 0x3000:
+		if chp.registers[x] == uint8(kk){
+			chp.pc += 2
+		}
+	case 0x1000:
+		chp.pc = nnn - 2
+	case 0xC000:
+		rand.Seed(time.Now().UnixNano())
+		chp.registers[x] = uint8(kk & uint16(rand.Int31n(256)))
 	default:
 		hexopc := fmt.Sprintf("%x", opcode)
 		fmt.Println("cheepcheep: bad opcode", hexopc)
@@ -94,7 +160,7 @@ func Interpret(chp *Cheep8){
 
 func main(){
 	var chp Cheep8
-	jit = true
+	jit = false
 	loadROM("pong.ch8", &chp)
 	for ;;{
 		Process(&chp)
--- recompiler.go
+++ recompiler.go
@@ -8,7 +8,7 @@ import(
 
 func AssembleAddition(kk uint16, vx uint16, chp *Cheep8){
 	FunctionCode := []uint16{
-		0x488b, 0x0425, *(*uint16)(unsafe.Pointer(&kk)), 0x0, 0x488b, 0x1425, *(*uint16)(unsafe.Pointer(&vx)), 0x0, 0x4801, 0xc2, 0x4889, 0x0425, *(*uint16)(unsafe.Pointer(&vx)), 0x0, 0x00c3,
+		0x488b, 0x0425, (uint16)(), 0x0, 0x488b, 0x1425, *(*uint16)(unsafe.Pointer(&vx)), 0x0, 0x4801, 0xc2, 0x4889, 0x0425, *(*uint16)(unsafe.Pointer(&vx)), 0x0, 0x00c3,
 	   }
 	memory, err := syscall.Mmap(-1, 0, 64, syscall.PROT_READ | syscall.PROT_WRITE | syscall.PROT_EXEC, syscall.MAP_PRIVATE | syscall.MAP_ANONYMOUS)
 	if err != nil{
